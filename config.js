6bc88a982faaedc3230c5d08feeaad39692492a9
Fran Varney <fran.varn@gmail.com>
2017-06-02 18:55:22 -0700

    Add Clarifai config

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  clarifai: {
+    client_id: process.env.CLARIFAI_CLIENT_ID || 'id',
+    client_secret: process.env.CLARIFAI_CLIENT_SECRET || 'secret'
+  },
   port: process.env.PORT || 8000
 };
